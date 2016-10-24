83bd66f09c9aeaa3b93880cdfa5d54f98e5fe186
sbluen <sbluen153@yahoo.com>
2016-10-23 20:26:04 -0700

    use the unit test class

--- tests/client_packet_test.py
+++ tests/client_packet_test.py
@@ -2,6 +2,7 @@ import threading
 import time
 import ConfigParser
 import urllib2
+import unittest
 
 class StoppableThread(threading.Thread):
     """Thread class with a stop() method. The thread itself has to check
@@ -16,35 +17,35 @@ class StoppableThread(threading.Thread):
 
     def stopped(self):
         return self._stop.isSet()
-
-def get_config():
-    global config
-    parser = ConfigParser.ConfigParser()
-    parser.read("config.txt")
-    config = parser._sections["test"]
     
-
-def _update(thread):
-    params = "token=123"
-    url = "%s:%s/?%s" % (config["host"], config["port"], params)
-    for i in range(1, 10+1):
-        if not thread.stopped():
-            time.sleep(0.1)
-            f = urllib2.urlopen(url)
-            print(f.read())
-            print "sent request %s" % i
-        else:
-            return
-    thread.stop()
+class TestPackets(unittest.TestCase):
+    def _update(self, thread):
+        params = "token=123"
+        url = "%s:%s/?%s" % (self.config["host"], self.config["port"], params)
+        for i in range(1, 10+1):
+            if not thread.stopped():
+                time.sleep(0.1)
+                f = urllib2.urlopen(url)
+                print(f.read())
+                print "sent request %s" % i
+            else:
+                return
+        thread.stop()
+        
+    def setUp(self):
+        parser = ConfigParser.ConfigParser()
+        parser.read("config.txt")
+        self.config = parser._sections["test"]
+        
+    def runTest(self):
+        thread = StoppableThread(self._update)
+        thread.start()
+        for i in range(1, 10+1):
+            if not thread.stopped():
+                time.sleep(1)
+                print str(i) + "second(s)"
+            
+        thread.stop()
 
 if __name__ == "__main__":
-    config = {}
-    get_config()
-    thread = StoppableThread(_update)
-    thread.start()
-    for i in range(1, 10+1):
-        if not thread.stopped():
-            time.sleep(1)
-            print str(i) + "second(s)"
-        
-    thread.stop()
+    unittest.main()
