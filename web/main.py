6d140e2d448affa3970e8e30032a5f7c7e689a98
sbluen <sbluen@hostus.(none)>
2016-10-29 23:20:46 -0400

    start web component

--- web/main.py
+++ web/main.py
@@ -11,5 +11,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/set")
+def hello():
+    return "Update request received"
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
