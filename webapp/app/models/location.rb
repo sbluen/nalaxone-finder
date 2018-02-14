93f8190ceff017a2dffaffd31ac538d92ddff536
sbluen <sbluen153@yahoo.com>
2018-02-14 01:51:04 -0500

    -stabilize verison of bootstrap since the newer versions are incompatable with the autogenerated code -remove incomplete rb code now handled elsewhere

--- webapp/app/models/location.rb
+++ webapp/app/models/location.rb
@@ -1,9 +1,2 @@
-retuire 'uri'
-
 class Location < ApplicationRecord
-    def get_coords (address)
-        url = "http://maps.google.com/maps/api/geocode/json?address=" + URI.escape(address)
-        html = open(url).read
-        json = js.
-    end
 end
