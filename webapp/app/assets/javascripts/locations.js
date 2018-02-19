0b777df390a70fd96d848e3c26026ed0997dc46f
sbluen <sbluen153@yahoo.com>
2018-02-19 16:05:25 -0500

    set textboxes with the javascript from the maps

--- webapp/app/assets/javascripts/locations.js
+++ webapp/app/assets/javascripts/locations.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    $.getScript("https://maps.googleapis.com/maps/api/js");
     $('.recalculate').focusout(populate_location);
 })
 
@@ -10,34 +9,53 @@ function populate_location(){
 
     if ((location_lat == "" || location_long == "") && location_address != ""){
         
-        console.log("coordinates should be set here")
+        console.log("coordinates should be set here");
+        url = "https://maps.googleapis.com/maps/api/geocode/json?address="+location_address;
+
+        $.getJSON(url, function(data){
+            location_lat = data.results[0].geometry.location.lat;
+            location_long = data.results[0].geometry.location.lng;
+            document.getElementById("location_lat").value = location_lat;
+            document.getElementById("location_long").value = location_long;
+        });
     }
 
     if ((location_lat != "" && location_long != "") && location_address == ""){
-        console.log("address should be set here");   
+        console.log("address should be set here");
+        url = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+location_lat+","+location_long;
+        $.getJSON(url, function(data){
+            location_address = data.results[0].formatted_address;
+            document.getElementById("location_address").value = location_address;
+        });
     }
 
 }
 
-
 //Using code derived from google api
 function initMap(){
     var myLatlng = {lat: 37.4220, lng: -122.0841};
-    var map = new google.maps.Map(document.getElementById('formmap'), {
+    var myMap = new google.maps.Map(document.getElementById('formmap'), {
         zoom: 4,
         center: myLatlng
     });
     var haveMarker = false;
     var marker
-    map.addListener('click', function(e){
+    myMap.addListener('click', function(e){
         if (haveMarker){
             marker.setMap(null);
+            haveMarker = false;
         }
+        //console.log(map as google.maps.Map);
         marker = new google.maps.Marker({
-          position: e.latLng,
-          map: map,
-          title: 'Selected position'
+          position: e.latLng.toJSON(),
+          map: myMap,
+          title: 'Selected position',
+          visible: true
         });
+        haveMarker = true;
+        document.getElementById("location_lat").value = marker.getPosition().lat();
+        document.getElementById("location_long").value = marker.getPosition().lng();
+        populate_location();
     })
 }
 
